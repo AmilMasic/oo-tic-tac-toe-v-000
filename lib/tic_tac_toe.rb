2d6f73e19864aed7ae8ab1c641af8ac0287ea4e3
Amil Masic <amil.masic@gmail.com>
2018-09-01 15:56:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,5 +22,8 @@ def display_board(board = nil)
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
+def input_to_index(user_input)
+  input = user_input .to_i - 1
+end
 
 end
